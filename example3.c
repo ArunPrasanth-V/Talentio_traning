ab986fcaf016c6d40f3f2700d6d0e06fa5d2ebb5
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-06 14:55:48 +0530

    Create example3.c

---
+++ example3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=5;
+    float c=5.5,d=7.5;
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%f\n",c+d);
+    printf("%f\n",c-d);
+
+    return 0;
+}
