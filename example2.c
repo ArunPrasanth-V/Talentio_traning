6e78af1b3f2fb2e5af0926187dc117c4d37799d6
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-06 13:00:33 +0530

    Create example2.c

---
+++ example2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+//b-a c-b 
+//equal
+// add or subtract
+
+void main()
+{
+     int num=13, flag=0;
+     while(num>5)
+     {
+         num-=5;
+         flag++;
+     }
+     printf("%d",flag);
+     
+}
